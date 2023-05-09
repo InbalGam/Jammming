2f5ccb982ec8ed33d5e93079c389dd56c75817c2
InbalGam <inbalgam@post.bgu.ac.il>
2023-05-09 11:11:53 +0300

    deleted comments

--- src/components/Tracklist.js
+++ src/components/Tracklist.js
@@ -2,15 +2,9 @@ import Track from "./Track";
 import styles from '../components/styles/Tracklist.css';
 
 function Tracklist(props) {
-
-    // function preview(i) {
-    //     let audio = props.tracks[i].preview_url;
-    // }
-
     return (
         props.tracks.map((el, ind) => 
                         <li key={ind}> 
-                        {/* <Track songName={el.songName} artist={el.artist} album={el.album} type={el.type} /> <button onClick={() => props.onButtonClick(el)}> {props.button} </button>  */}
                         <div className="track_div"> <Track songName={el.songName} artist={el.artist} album={el.album} type={el.type} /> <audio src={el.preview_url} controls/> <button onClick={() => props.onButtonClick(el)} className="tracklistButton"> {props.button} </button> </div>
                         </li>)
     );
