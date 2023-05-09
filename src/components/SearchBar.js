b1cd265e2ad28269409e85326cc88c946d5a44bd
InbalGam <inbalgam@post.bgu.ac.il>
2023-05-09 09:43:48 +0300

    added a note for when searching for artist

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -6,6 +6,7 @@ import styles from '../components/styles/SearchBar.css';
 function SearchBar(props) {
     const [text, setText] = useState('');
     const [searchType, setSearchType] = useState('track');
+    const [error, setError] = useState('');
     const options = [{value: 'track', label: 'Track name'}, {value: 'artist', label: 'Artist name'}];
 
     function handleTextChange(e) {
@@ -17,6 +18,7 @@ function SearchBar(props) {
        event.preventDefault();
        const results = await searchSpotify(text, searchType, props.token);
        props.getTracks(results);
+       {searchType === 'artist' ? setError('Please note, you cannot save an artist playlist') : setError('')}
     };
 
 
@@ -32,6 +34,7 @@ function SearchBar(props) {
                 <input id='search' type='text' value={text} onChange={handleTextChange} name='search' className='search_input'/>
                 <Select defaultValue={options[0]} options={options} onChange={handleSelectChange} className='select' />
                 <input type="submit" value="Submit" className='submit' />
+                <p className='error'>{error}</p>
             </form>
         </div>
     );
